c3078bdc919f69bc57282f38e09133f8307723c0
Robert Smith <rbrtsmith84@googlemail.com>
2017-07-12 09:23:29 +0100

    fix regex, tidy up npm scripts

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -1,7 +1,7 @@
 import { loadPackageJSON, mergeDeep } from './../utils';
 
 const hookPrefix = 'redibox-hook';
-const hookRegexReplace = new RegExp(`@?[a-zA-Z-_0-9.]*?\/?${hookPrefix}-`);
+const hookRegexReplace = new RegExp(`@?[a-zA-Z-_0-9.]*?${hookPrefix}-`);
 
 /**
  *
